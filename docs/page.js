329dd74a6e0b3a2f480e567ae015a38d42554535
Jonas Wagner <ltlygwayh@gmail.com>
2022-03-15 08:42:48 +0100

    Add an input field for the entropy.

--- docs/page.js
+++ docs/page.js
@@ -1,21 +1,31 @@
-function getRandomSeed() {
+function getEntropy() {
     const bytes = new Uint8Array(32);
     crypto.getRandomValues(bytes);
     return Array.from(bytes).map(b => ('0' + b.toString(16)).slice(-2)).join('');
 }
 
 function getArguments() {
-    const seed = ["--deterministic", getRandomSeed()];
+    const seed = ["--deterministic", document.getElementById('entropy').value];
     const input = document.getElementById("input").value;
     const arguments = input ? seed.concat(input.split(/\s+/)) : seed;
-    console.log("arguments:", arguments);
     return arguments;
 }
 
-function setArgumentsAndRun() {
-    Module.arguments = getArguments();
+function runSeedtool() {
+    const arguments = getArguments();
+    console.log("arguments:", arguments);
+
     document.getElementById('output').textContent = "";
-    callMain(Module.arguments);
+
+    callMain(arguments);
+}
+
+function waitForSeedtoolAndRun() {
+    if (typeof callMain === 'undefined') {
+        setTimeout(waitForSeedtoolAndRun, 100);
+    } else {
+        runSeedtool();
+    }
 }
 
 var Module = {};
@@ -27,12 +37,15 @@ Module.print = (function () {
         element.textContent += text + "\n";
     };
 })();
-Module.arguments = getArguments();
+Module.noInitialRun = true;
 
 window.addEventListener('load', () => {
     const seedtoolScript = document.createElement('script');
     seedtoolScript.src = "seedtool.js";
     document.body.appendChild(seedtoolScript);
 
-    document.getElementById('lets-go').addEventListener('click', setArgumentsAndRun);
+    document.getElementById('entropy').value = getEntropy();
+
+    document.getElementById('lets-go').addEventListener('click', runSeedtool);
+    setTimeout(waitForSeedtoolAndRun, 0);
 });
