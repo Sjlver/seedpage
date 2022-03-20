ae867a12b85ce67fb50fba836986a4e3084249d3
Jonas Wagner <ltlygwayh@gmail.com>
2022-03-20 21:54:11 +0100

    Better handling for --group; display stderr.

--- docs/page.js
+++ docs/page.js
@@ -1,6 +1,7 @@
 var Module = {};
 Module.noInitialRun = true;
 Module.print = printToElement;
+Module.printErr = printErr;
 Module.outputElement = null;
 
 function getEntropy() {
@@ -16,6 +17,15 @@ function getArguments() {
     return arguments;
 }
 
+function setBackgroundColor(element, color) {
+    var currentColor;
+    for (c of element.classList) {
+        if (/^bg-/.test(c)) currentColor = c;
+    }
+    if (currentColor) element.classList.remove(currentColor);
+    element.classList.add(color);
+}
+
 function runSeedtool() {
     const arguments = getArguments();
     console.log("arguments:", arguments);
@@ -23,6 +33,13 @@ function runSeedtool() {
     for (const format of ["hex", "btw", "bip39", "sskr"]) {
         Module.outputElement = document.getElementById(`output-${format}`)
         Module.outputElement.textContent = "";
+
+        if (arguments.some(arg => /^--group/.test(arg)) && format != "sskr") {
+            setBackgroundColor(Module.outputElement.parentElement, "bg-neutral-200/50");
+            continue;
+        }
+
+        setBackgroundColor(Module.outputElement.parentElement, "bg-blue-200/50");
         callMain(arguments.concat([`--out=${format}`]))
     }
 }
@@ -39,6 +56,12 @@ function printToElement(text) {
     if (arguments.length > 1) text = Array.prototype.slice.call(arguments).join(' ');
     console.log(text);
     Module.outputElement.textContent += text + "\n";
+}
+
+function printErr(text) {
+    if (/^program exited \(with status:/.test(text)) return;
+    printToElement(text);
+    setBackgroundColor(Module.outputElement.parentElement, "bg-red-200/50");
 }
 
 window.addEventListener('load', () => {
